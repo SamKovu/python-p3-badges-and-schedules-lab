da77f0aef3b9c17ee186ba4b2e55aaca26071449
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-12-05 01:27:25 +0300

    Completed lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,34 @@
 def badge_maker(name):
-    return None
+    message = "Hello, my name is " + name + "."
+    return message
+
+"""
+def batch_badge_creator(names):
+    myList=list()
+    for name in names:
+        myList.append(badge_maker(name))
+    return myList
+"""
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+
 
 def assign_rooms(names):
-    return None
+    myList=list()
+    i=1
+    for name in names:
+        myList.append(f"Hello, {name}! You'll be assigned to room {i}!")
+        i+=1
+    return myList
+
+
+
 
 def printer(names):
+    for x in batch_badge_creator(names):
+        print(x)
+    for x in assign_rooms(names):
+        print(x)
+
     return None
